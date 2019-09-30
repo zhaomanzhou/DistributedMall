b5ac32121922b4634ff7db48896027563d396d9d
weiqiangpro <280803590@qq.com>
2019-09-30 16:53:03 +0800

    wq

--- web/src/test/java/test.java
+++ web/src/test/java/test.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class test {
-//    public static void main(String[] args) throws Exception{
-//        List<String> warnings = new ArrayList<String>();
-//        boolean overwrite = true;
-//        File configFile = new File("mbg.xml");
-//        ConfigurationParser cp = new ConfigurationParser(warnings);
-//        Configuration config = cp.parseConfiguration(configFile);
-//        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-//        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-//        myBatisGenerator.generate(null);
-//    }
+    public static void main(String[] args) throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
 }
