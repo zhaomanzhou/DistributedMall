ccf76ee04f7c62f5b76250d433e1e9f42aedec1c
zmz <zmzsstreet@163.com>
2019-10-18 08:43:34 +0800

    aaaa

---
+++ dao/src/main/java/com/mmall/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mmall.mapper;
+
+import com.mmall.bean.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
