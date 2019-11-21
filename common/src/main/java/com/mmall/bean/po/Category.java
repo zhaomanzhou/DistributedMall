f4eb9e0fbc89cd702a2816306ff01ebbdd525808
weiqiangpro <280803590@qq.com>
2019-11-21 16:36:15 +0800

    wq

---
+++ common/src/main/java/com/mmall/bean/po/Category.java
@@ -0,0 +1,28 @@
+package com.mmall.bean.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String name;
+
+    private Boolean status;
+
+    private Integer sortOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
---
+++ dao/src/main/java/com/mmall/mapper/ProductMapper.java
@@ -0,0 +1,28 @@
+package com.mmall.mapper;
+
+import com.mmall.bean.po.Product;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+
+
+    List<Product> selectList();
+
+    List<Product> selectByNameAndProductId(@Param("productName")String productName, @Param("productId") Integer productId);
+
+    List<Product> selectByNameAndCategoryIds(@Param("productName")String productName,@Param("categoryIdList")List<Integer> categoryIdList);
+
+    //这里一定要用Integer，因为int无法为NULL，考虑到很多商品已经删除的情况。
+    Integer selectStockByProductId(Integer id);
+}
